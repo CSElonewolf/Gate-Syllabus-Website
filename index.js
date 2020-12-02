02992773a683ef43c209d342f6da515a612a3216
Devkant Gorai <devkantgorai1@gmail.com>
2020-12-02 14:34:07 +0530

    Today's Routine section has been added with svgs

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function veiwpdf(val) {
 	obj.setAttribute("data", val);
 	console.log(obj.getAttribute("data"))
 }
+var date = new Date();
+date = date.getDate();
+if (date % 2 == 1) {
+	document.getElementById("subjects").innerHTML = `<li><img src="gate.svg" style="width:20px"> GATE LECTURES</li><li><img src="gfg.svg" style="width:20px"> GFG's Videos</li>
+<li><img src="python.svg" style="width:20px"> PYTHON VIDEOS</li><li><img src="css.svg" style="width:20px"> CSS VIDEOS</li>`
+}
+else if (date % 2 == 0) {
+	document.getElementById("subjects").innerHTML = `<li><img src="maths.svg" style="width:20px"> MATHS</li><li><img src="dld.svg" style="width:20px"> DIGITAL LOGIC</li>
+	<li><img src="gate.svg" style="width:20px"> GATE LECTURES</li><li><img src="python.svg" style="width:20px"> PYHTON VIDEOS</li>`
+}
