2abbc09229acb62bba581a77df4d511119b5d326
Devkant Gorai <devkantgorai1@gmail.com>
2020-12-04 22:16:29 +0530

    Added the routine for sunday and fixed some typo

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ function veiwpdf(val) {
 	console.log(obj.getAttribute("data"))
 }
 var date = new Date();
+day = date.getDay();
+console.log(day)
 date = date.getDate();
-if (date % 2 == 1) {
-	document.getElementById("subjects").innerHTML = `<li><img src="gate.svg" style="width:20px"> GATE LECTURES</li><li><img src="gfg.svg" style="width:20px"> GFG's Videos</li>
-<li><img src="python.svg" style="width:20px"> PYTHON VIDEOS</li><li><img src="css.svg" style="width:20px"> CSS VIDEOS</li>`
+if (date % 2 == 1 && day !=0) {
+	document.getElementById("subjects").innerHTML =
+	 `<li><img src="gate.svg" style="width:20px"> GATE LECTURES</li>
+	<li><img src="gfg.svg" style="width:20px"> GFG's Videos</li>
+<li><img src="python.svg" style="width:20px"> PYTHON VIDEOS</li>
+<li><img src="css.svg" style="width:20px"> CSS VIDEOS</li>`
 }
-else if (date % 2 == 0) {
-	document.getElementById("subjects").innerHTML = `<li><img src="maths.svg" style="width:20px"> MATHS</li><li><img src="dld.svg" style="width:20px"> DIGITAL LOGIC</li>
-	<li><img src="gate.svg" style="width:20px"> GATE LECTURES</li><li><img src="python.svg" style="width:20px"> PYTHON VIDEOS</li>`
+else if (date % 2 == 0 && day !=0) {
+	document.getElementById("subjects").innerHTML =
+	`<li><img src="maths.svg" style="width:20px"> MATHS</li>
+	<li><img src="dld.svg" style="width:20px"> DIGITAL LOGIC</li>
+	<li><img src="gate.svg" style="width:20px"> GATE LECTURES</li>
+	<li><img src="python.svg" style="width:20px"> PYTHON VIDEOS</li>`
+}
+else{
+	document.getElementById("subjects").innerHTML =
+	 `<li><img src="cp.png" style="width:20px"> COMPETITIVE CODING</li>
+	<li><img src="gate.svg" style="width:20px"> GATE LECTURES</li>`
 }
